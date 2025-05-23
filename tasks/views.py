f749c934ad88c4e57e88a1f7b51bfad112f63419
Abdelkader Naji <abdelkader.naji@fujitsu.com>
2025-05-23 15:03:38 +0200

    Updates

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')  # order by DESC
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
